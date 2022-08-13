8ae05abcecb0cc53fffca79c59ca38e34a148ff2
M2kishore <kishoremmofficial@gmail.com>
2022-08-13 23:16:28 +0530

    deleted old files

--- old/README.md
+++
@@ -1,4 +0,0 @@
-# netcentric_project
-# Bug tracking System
-
-This project aims to create a bug tracker to register bugs prevailing and resolve them.
--- old/index.js
+++
@@ -1,8 +0,0 @@
-function openNavItems() {
-    var x = document.getElementById("topnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
